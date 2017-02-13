d2ec9fd8723cebed786e113cf170636abbea4ab6
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2017-02-12 21:12:42 -0700

    media queries finished, added jquery mediaquery code, top navbar color change at >1024

--- main.js
+++ main.js
@@ -1,7 +1,68 @@
 $(document).ready(function() {
-});
+
+  var changed = false;
+  var handler = true;
+
+  var menuicon = $('#menuicon');
+  var navcontainer = $('.navcontainer');
+  var scrollElement = $('.herocontainer');
+  var woticon = $('.woticon');
+  var header = $('.header');
+  var a =$('a');
+
+  var scrolltop = 0;
 
 
-$('.menuicon').click(function(){
-  $('.navcontainer').toggleClass('active');
+  function checkSize(){
+    console.log($(window).width());
+
+    if($(window).width() >= 1024){
+      menuicon.off();
+      menuicon.hide();
+      navcontainer.show();
+      handler = false;
+    }else{
+      navcontainer.hide();
+
+      if(!handler){
+        menuicon.click(function(){
+            navcontainer.slideToggle();
+        });
+        menuicon.show();
+        handler = true;
+      }
+    }
+  }
+
+  $(window).bind( "scroll", function(e) {
+    // When the window is scrolled, update the scroll position output
+    scrolltop = $(window).scrollTop();
+    console.log(handler, scrolltop);
+    if(handler !== false)
+      return;
+    else if(scrolltop !== 0 && changed === false){
+      header.css({'background-color': 'white'});
+      woticon.css("background-image", "url('img/wot-logo-dark.svg')");
+      a.css("color", "#6A787C");
+      changed = true;
+    }else if(scrolltop === 0 && changed === true){
+      header.css('background-color', 'transparent');
+      a.css("color", "white");
+      woticon.css("background-image", "url('img/title_wideopentech.png')");
+
+      changed = false;
+    }
+
+  });
+
+
+  menuicon.click(function(){
+      navcontainer.slideToggle();
+  });
+
+  checkSize();
+
+  $(window).resize(checkSize);
+
+
 });
